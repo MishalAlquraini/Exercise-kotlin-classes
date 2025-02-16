23871b3418c023ab13729959567f498f6015dac2
Mishal Alquraini <alqurainimishal@gmail.com>
2025-02-16 18:34:10 +0300

    done with part 2

--- Task part 1/src/Main.kt
+++ Task part 1/src/Main.kt
@@ -1,18 +1,33 @@
 
-class Book{
-    var title: String = ""
-    var author: String = ""
-    var yearPublished: Int = 2000
+// title after book = its a requirement (constructor
+// it can be after Book or write constructor
+class Book(var title: String, var author: String, var yearPublished: Int){
+//    var title: String = ""
+//    var author: String = ""
+//    var yearPublished: Int = 2000
+//    constructor(title: String, author: String, yearPublished: Int)
 
-//    fun info(): String{
-//        return "Title: $title Author: "
-//    }
+    fun info(){
+        println("title= $title, author: $author, Published: $yearPublished")
+    }
+}
+// name = primary constructor , accNum is a secondary constructor
+class bankAcc(var name: String) {
+    constructor( accountNum: Int): this("")
+    fun printInfo(){
+        println("Account Holder: $name")
+    }
 }
 
-fun main() {
-    var myBook = Book()
 
-    myBook.author = "Mishal"
-    myBook.title = "Journey"
-    myBook.yearPublished = 2025
+fun main() {
+   var nbkAcc = bankAcc("Mishal")
+    nbkAcc.printInfo()
+
+    var myBook = Book("Journey", "Mishal", 20205)
+//    myBook.author = "Mishal"
+//    myBook.title = "Journey"
+//    myBook.yearPublished = 2025
+    myBook.info()
+
 }
